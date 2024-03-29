0738c193ccdb78e6637bd683588aab7032049d2c
William Alonso <williamalonso@outlook.com>
2024-03-28 22:54:44 -0300

    :sparkles: att

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import Navbar from "@/components/Navbar/navbarComponent";
 import PageTitle from "@/components/Title/pageTitleComponent";
+import Datatable from "@/components/Datatable/datatableComponent";
 
 const Home = () => {
   return (
     <>
       <PageTitle title="Excel" />
       <Navbar />
-      <div>Home page</div>
+      <Datatable />
     </>
   );
 }
