06cb5c22bc7cca85a0350ed830b5c48ba3e9d83d
William Alonso <williamalonso@outlook.com>
2024-04-11 23:03:32 -0300

    :zap: att

--- src/components/Excel/excelComponent.tsx
+++ src/components/Excel/excelComponent.tsx
@@ -13,17 +13,17 @@ interface ExportToExcelButtonProps {
   columns?: { label: string; value: string }[] | undefined;
 }
 
-const exportButtonElement = (
-  <div className="container mx-auto flex justify-end items-center my-4">
-    <div className="flex rounded-[30px] bg-[#f5f5f5] w-40 h-12 items-center  text-[18px] font-medium cursor-pointer
-    shadow-[0_4px_6px_-1px_rgba(0,0,0,.1),0_2px_4px_-1px_rgba(0,0,0,.06)] justify-evenly">
-      <span className="text-[#2587e1] bg-[#f5f5f5]">Exportar</span>
-      <Image src="/images/vectorExcelArrow.svg" alt="Seta de download com ícone para cima" width={22} height={22} className="bg-[#f5f5f5]" />
-    </div>
-  </div>
-);
-
 const ExportToExcelButton: React.FC<ExportToExcelButtonProps> = ({ data, fileName, sheetName, columns }) => {
+
+  const exportButtonElement = (
+    <div className="container mx-auto flex justify-end items-center my-4">
+      <div className="flex rounded-[30px] bg-[#f5f5f5] w-40 h-12 items-center  text-[18px] font-medium cursor-pointer
+      shadow-[0_4px_6px_-1px_rgba(0,0,0,.1),0_2px_4px_-1px_rgba(0,0,0,.06)] justify-evenly">
+        <span className="text-[#2587e1] bg-[#f5f5f5]">Exportar</span>
+        <Image src="/images/vectorExcelArrow.svg" alt="Seta de download com ícone para cima" width={22} height={22} className="bg-[#f5f5f5]" />
+      </div>
+    </div>
+  );
 
   return (
     <ExcelFile 
